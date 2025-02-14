6efb40c9babe86dbf71cbdb855956f886f322cf4
ankeatacodes <ankitaghoshsep@gmail.com>
2025-02-14 22:32:53 +0530

    Fixed server.js modification issue

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+// App Config
+const app= express()
+const port = process.env.PORT || 4000
+
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.get ('/', (req,res) => {
+    res.send ("API working")
+})
+
+app.listen (port, ()=>{
+    console.log ('Server started on PORT :' +port)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
